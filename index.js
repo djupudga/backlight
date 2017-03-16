620a657f2556fbbfc51c45d2f461d2ea246cadca
Helgi Kristjansson <helgi.kristjansson@gmail.com>
2017-03-16 10:15:53 +0100

    Added support for percentage

--- index.js
+++ index.js
@@ -8,21 +8,38 @@ const BRIGHTNESS_FILE =
 	'/sys/class/backlight/intel_backlight/brightness'
 // './brightness'
 
-const cmd = process.argv[2]
-assertCmd(cmd)
+const arg = process.argv[2]
+const command = assertCmd(arg)
 
-if (cmd == 'inc') {
-	setBrightness(5)
-} else if (cmd == 'dec') {
-	setBrightness(-5)
+if (command.inc) {
+	setBrightness(calculateBrightnessChange(5))
+} else if (command.dec) {
+	setBrightness(calculateBrightnessChange(-5))
+} else {
+	setBrightness(
+		calculateBrightness(
+			parseInt(command.percentage, 10)
+		)
+	)
 }
 
-function setBrightness(value) {
+function calculateBrightness(percentage) {
+	const maxBrightness = getMaxBrightness()
+	const step = calculateStep(maxBrightness)
+	return parseInt(step * percentage, 10)
+}
+
+function calculateBrightnessChange(value) {
 	const maxBrightness = getMaxBrightness()
 	const step = calculateStep(maxBrightness)
 	const current = getBrightness()
 
 	let newBrightness = parseInt(current + (step * value), 10)
+	return newBrightness
+}
+
+function setBrightness(newBrightness) {
+	const maxBrightness = getMaxBrightness()
 
 	// Validate value and check bounds
 	if (isNaN(newBrightness)) {
@@ -54,10 +71,25 @@ function getMaxBrightness() {
 }
 
 function assertCmd(cmd) {
-	if (!['inc', 'dec'].includes(cmd)) {
-		console.error('Error: Wrong arguments')
-		usage()
-		process.exit(1)
+	let percentage = parseInt(cmd, 10)
+	if (isNaN(percentage)) {
+		percentage = false
+		if (!['inc', 'dec'].includes(cmd)) {
+			console.error('Error: Wrong arguments')
+			usage()
+			process.exit(1)
+		}
+	} else {
+		if (percentage < 0 || percentage > 100) {
+			console.error('Error: Brightness value must be between 0 and 100')
+			usage()
+			process.exit(1)
+		}
+	}
+	return {
+		inc: cmd == 'inc',
+		dec: cmd == 'dec',
+		percentage: percentage
 	}
 }
 
@@ -67,7 +99,8 @@ Usage:
 	backlight [COMMAND]
 
 COMMAND:
-	inc   Increase bightness by 5%
-	dec   Decrease brightness by 5%
+	inc     Increase bightness by 5%
+	dec     Decrease brightness by 5%
+	0-100   Brightness percentage between 0% and 100%
 `) // eslint-disable-line
 }
